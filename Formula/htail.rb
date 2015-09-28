b39c5eee5c0d761bdaad78adb886f749e2c0e7f7
Marc Weistroff <marc@weistroff.net>
2015-09-28 16:03:31 +0200

    Update htail to v0.0.3

--- Formula/htail.rb
+++ Formula/htail.rb
@@ -6,8 +6,8 @@ class Htail < Formula
     homepage 'https://github.com/delatech/htail'
     version '0.0.3'
 
-    url 'https://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.3.tar.gz'
-    sha1 'https://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.3.sha1'
+    url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.3.tar.gz'
+    sha1 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.3.sha1'
 
     depends_on :arch => :intel
 
