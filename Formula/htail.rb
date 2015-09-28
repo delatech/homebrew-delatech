4a118b6ce579bc78ba0b279b98a581f6591fbaa3
Marc Weistroff <marc@weistroff.net>
2015-09-28 16:16:36 +0200

    Update htail to v0.0.3

--- Formula/htail.rb
+++ Formula/htail.rb
@@ -7,11 +7,11 @@ class Htail < Formula
     version '0.0.3'
 
     url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.3.tar.gz'
-    sha1 '994d8827b6ba896cb7fee672bd9f2c0f889611d6'
+    sha1 '2e40af42ad3d7ee18dedc777a7034ff89d90805d'
 
     depends_on :arch => :intel
 
     def install
-        bin.install 'usr/bin/htail'
+        bin.install 'bin/htail'
     end
 end
