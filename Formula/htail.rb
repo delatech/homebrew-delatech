4701271625aeb0d0f5f964e70804c0edd7860c0f
Marc Weistroff <marc@weistroff.net>
2015-09-28 18:12:55 +0200

    Update htail to v0.0.8

--- Formula/htail.rb
+++ Formula/htail.rb
@@ -4,10 +4,10 @@ require 'formula'
 
 class Htail < Formula
     homepage 'https://github.com/delatech/htail'
-    version '0.0.7'
+    version '0.0.8'
 
-    url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.7.tar.gz'
-    sha1 'af2f4dc3f6646eac44b0677907e274751b847d87'
+    url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/htail/htail-0.0.8.tar.gz'
+    sha1 'b8f4ba598a435b6d28def31d3c5a9a94b2e3e37c'
 
     depends_on :arch => :intel
 
