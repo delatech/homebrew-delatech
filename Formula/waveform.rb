0869a2335e6f410b84b0853e251b6da4f7c36cf2
Marc Weistroff <marc@weistroff.net>
2016-01-27 10:26:50 +0100

    Update waveform to v0.3.2

--- Formula/waveform.rb
+++ Formula/waveform.rb
@@ -7,7 +7,7 @@ class Waveform < Formula
     version '0.3.2'
 
     url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/waveform/waveform-0.3.2.tar.gz'
-    sha1 '09c11468b017331845c702fd0fd3d2b635079e5f'
+    sha1 'e604e91b45d79ced86ad92a415c9f24efd962cda'
 
     depends_on :arch => :intel
 
