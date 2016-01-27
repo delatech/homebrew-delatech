ad2de123d9c6f35d6b0ac034b1daf765e60be2d7
Marc Weistroff <marc@weistroff.net>
2016-01-27 11:00:12 +0100

    Update waveform to v0.3.4

--- Formula/waveform.rb
+++ Formula/waveform.rb
@@ -4,10 +4,10 @@ require 'formula'
 
 class Waveform < Formula
     homepage 'https://github.com/delatech/waveform'
-    version '0.3.3'
+    version '0.3.4'
 
-    url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/waveform/waveform-0.3.3.tar.gz'
-    sha1 'd5177f20c72f2e0c3cf60dfb9883fca953f0f82c'
+    url 'http://release.delatech.net.s3-website-eu-west-1.amazonaws.com/waveform/waveform-0.3.4.tar.gz'
+    sha1 'e055703640a40545f0a8323cd60e3091fd14691b'
 
     depends_on :arch => :intel
 
